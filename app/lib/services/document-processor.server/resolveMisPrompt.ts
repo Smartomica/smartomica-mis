53e836ef090c65e239312970d153d62e3337a51c
Boris Valdman <mghead@yandex.ru>
2026-03-03 11:56:28 +0200

    prompt resolver refactor

--- app/lib/services/document-processor.server/resolveMisPrompt.ts
+++ app/lib/services/document-processor.server/resolveMisPrompt.ts
@@ -1,4 +1,5 @@
 import type { ChatPromptClient, TextPromptClient } from "@langfuse/client";
+import { PromptType } from "@langfuse/core";
 import { ProcessingMode } from "~/generated/client/enums";
 import { NeverError } from "~/lib/error";
 import {
@@ -6,49 +7,63 @@ import {
   getLangfuseSDK,
   listPrompts,
 } from "~/lib/langfuse.server";
-import type { Lang, SimplifiedChatMessage } from "./const";
 import { SUPPORTED_LANGUAGES } from "~/types/document";
-import { PromptType } from "@langfuse/core";
+import type { Lang, SimplifiedChatMessage } from "./const";
+
+enum Tag {
+  ProjectMis = "mis",
+
+  OperationOcr = "ocr",
+  OperationTranslate = "translate",
+  OperationSummary = "summary",
+  OperationOnco = "oncology",
+
+  Glossary = "glossary",
+  TranslateLang = "translate-lang",
+  TranslateLegacy = "translate",
+  FromAny = "from-any-lang",
+  ToAny = "to-any-lang",
+}
+
+type LookupTag = Tag | Lang;
 
 export async function resolveMisPrompt(
   processingMode: ProcessingMode,
-  sourceLanguage: Lang,
-  targetLanguage: Lang,
+  sourceLang: Lang,
+  targetLang: Lang,
 ): Promise<SimplifiedChatMessage[]> {
-  const languageSetting = [sourceLanguage, targetLanguage] as const;
-  const misPrompts = await listPrompts({ tag: "mis", limit: 100 });
+  const languageSetting = [sourceLang, targetLang] as const;
+  const misPrompts = await listPrompts({
+    tag: Tag.ProjectMis,
+    limit: 100,
+  });
 
-  const glossarySourceTextPromptName = getTextName("glossary", sourceLanguage);
-  const glossaryTargetTextPromptName = getTextName("glossary", targetLanguage);
-  const glossarySourceTextPrompt = await getPrompt(
-    PromptType.Text,
-    glossarySourceTextPromptName,
-  );
-  const glossaryTargetTextPrompt = await getPrompt(
-    PromptType.Text,
-    glossaryTargetTextPromptName,
-  );
-
-  function getChatName(...tags: string[]) {
-    return misPrompts.data
-      .filter(
-        (p) => p.type === "chat" && tags.every((tag) => p.tags.includes(tag)),
-      )
-      ?.at(0)?.name;
-  }
-
-  function getTextName(...tags: string[]) {
-    return misPrompts.data
-      .filter(
-        (p) => p.type === "text" && tags.every((tag) => p.tags.includes(tag)),
-      )
-      ?.at(0)?.name;
-  }
+  const [
+    glossarySourceTextPrompt,
+    glossaryTargetTextPrompt,
+    translationFromTextPrompt,
+    translationToTextPrompt,
+  ] = await Promise.all([
+    getPrompt(PromptType.Text, getTextName(Tag.Glossary, sourceLang)),
+    getPrompt(PromptType.Text, getTextName(Tag.Glossary, targetLang)),
+    getPrompt(
+      PromptType.Text,
+      getTextName(Tag.TranslateLang, Tag.FromAny, sourceLang) ||
+        getTextNameStrict(Tag.TranslateLang, Tag.ToAny),
+    ),
+    getPrompt(
+      PromptType.Text,
+      getTextName(Tag.TranslateLang, Tag.FromAny, targetLang) ||
+        getTextNameStrict(Tag.TranslateLang, Tag.FromAny),
+    ),
+  ]);
 
   switch (processingMode) {
     case ProcessingMode.OCR:
-      const ocrChatPromptName = getChatName("ocr");
-      const ocrChatPrompt = await getPrompt(PromptType.Chat, ocrChatPromptName);
+      const ocrChatPrompt = await getPrompt(
+        PromptType.Chat,
+        getChatName(Tag.OperationOcr),
+      );
 
       return combinePrompts(
         languageSetting,
@@ -57,10 +72,9 @@ export async function resolveMisPrompt(
       );
 
     case ProcessingMode.SUMMARISE:
-      const summariseChatPromptName = getChatName("summary");
       const summariseChatPrompt = await getPrompt(
         PromptType.Chat,
-        summariseChatPromptName,
+        getChatName(Tag.OperationSummary),
       );
 
       return combinePrompts(
@@ -70,11 +84,9 @@ export async function resolveMisPrompt(
       );
 
     case ProcessingMode.SUMMARISE_ONCO:
-      const summariseOncoChatPromptName = getChatName("oncology");
-
       const summariseOncoChatPrompt = await getPrompt(
         PromptType.Chat,
-        summariseOncoChatPromptName,
+        getChatName(Tag.OperationOnco),
       );
 
       return combinePrompts(
@@ -89,15 +101,15 @@ export async function resolveMisPrompt(
         misPrompts.data
           .filter(
             (p) =>
-              p.type === "chat" &&
-              p.tags.includes("translate") &&
+              p.type === PromptType.Chat &&
+              p.tags.includes(Tag.TranslateLegacy) &&
               // Require name to include smth like ru->en
-              p.name.indexOf(sourceLanguage) < p.name.indexOf(targetLanguage) &&
-              p.tags.includes(sourceLanguage) &&
-              p.tags.includes(targetLanguage),
+              p.name.indexOf(sourceLang) < p.name.indexOf(targetLang) &&
+              p.tags.includes(sourceLang) &&
+              p.tags.includes(targetLang),
           )
           .at(0)?.name ||
-        getChatName("translate", "from-any-lang", targetLanguage);
+        getChatName(Tag.TranslateLegacy, Tag.FromAny, targetLang);
 
       const langPairChatPrompt = await getPrompt(
         PromptType.Chat,
@@ -112,6 +124,46 @@ export async function resolveMisPrompt(
       );
     default:
       throw new NeverError(processingMode);
+  }
+
+  /**
+   * @desc: Resolves chat prompt by tag list. Extra tags are allowed
+   */
+  function getChatName(...tags: LookupTag[]) {
+    return misPrompts.data
+      .filter(
+        (p) =>
+          p.type === PromptType.Chat &&
+          tags.every((tag) => p.tags.includes(tag)),
+      )
+      ?.at(0)?.name;
+  }
+
+  /**
+   * @desc: Resolves chat prompt by tag list. Extra tags are allowed
+   */
+  function getTextName(...tags: LookupTag[]) {
+    return misPrompts.data
+      .filter(
+        (p) =>
+          p.type === PromptType.Text &&
+          tags.every((tag) => p.tags.includes(tag)),
+      )
+      ?.at(0)?.name;
+  }
+
+  /**
+   * @desc: Resolves chat prompt by tag list. No extra tags allowed
+   */
+  function getTextNameStrict(...tags: LookupTag[]) {
+    return misPrompts.data
+      .filter(
+        (p) =>
+          p.type === PromptType.Text &&
+          tags.every((tag) => p.tags.includes(tag)) &&
+          tags.length === p.tags.length,
+      )
+      ?.at(0)?.name;
   }
 }
 
